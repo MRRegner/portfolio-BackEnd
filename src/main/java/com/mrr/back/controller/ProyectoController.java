3347f3ff54b83123fca54f10cfe834c6dba79704
MRRegner <rodriguez.regner@gmail.com>
2022-11-12 22:03:39 -0300

    Cambio CrossOrigin en ProyectoController

--- src/main/java/com/mrr/back/controller/ProyectoController.java
+++ src/main/java/com/mrr/back/controller/ProyectoController.java
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins ="https://portfolio-f603c.web.app/")
-@CrossOrigin(origins ="http://localhost:4200/")
+@CrossOrigin(origins ="https://portfolio-f603c.web.app/")
+//@CrossOrigin(origins ="http://localhost:4200/")
 @RestController
 
 public class ProyectoController {
